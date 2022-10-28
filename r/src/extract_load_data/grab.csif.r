1f0d26deacc1f0fc4995ccdebfb226535d594f21
Sabrina <sabrina.madsen@mail.utoronto.ca>
2022-10-28 17:22:28 -0400

    #Adjusted to set SIF=0 in the winter

--- r/src/extract_load_data/grab.csif.r
+++ r/src/extract_load_data/grab.csif.r
@@ -3,35 +3,31 @@
 
 # timestr in form of YYYYMMDDHH
 # ext: generated from raster::extent(), e.g., minlon, maxlon, minlat, maxlat
-grab.csif <- function(csif.path, timestr, sif.temp, TA.path, TA.varname, 
+grab.csif <- function(csif.path, timestr, sif.temp, TA.path, TA.varname,
                       ext = NULL, nhrs = NULL,
                       var = c('clear_inst_SIF', 'clear_daily_SIF', 
                               'all_inst_SIF', 'all_daily_SIF')[2], 
-                      form = c('df', 'raster')[2],yr=2018) {
+                      form = c('df', 'raster')[2], yr2=2018) {
     
     # call a funcion to get DOY from YYYYMMDD, given day intervals
     if (nchar(timestr) != 10) stop('grab.csif(): timestr needs to be in form of YYYYMMDDHH')
     time.info <- get.doy.from.timestr(timestr, nday = 4, nhrs = nhrs)
     search.timestr <- time.info$find.yrdoy
-    
-    if ((search.timestr-yr*1000)<90 | (search.timestr-yr*1000)>340){
-      csif.file <- "C:/Users/kitty/Documents/Research/SIF/SMUrF/data/TROPOSIF/winter_sif.nc"
-    
-    }else{
-      
-      # select the ones that need for scaling GPP 
-      csif.files <- list.files(path = csif.path, pattern = '.nc', 
-                               recursive = T, full.names = T)
-      if (is.null(nhrs)) {
-        csif.file <- csif.files[grep(search.timestr, csif.files)]
 
-      } else {
-        ref.csif.files <- gsub('.v2', '', csif.files)
-        file.timestr <- strsplit.to.df(basename(ref.csif.files), sep = '\\.')$V5
-        csif.file <- csif.files[file.timestr >= search.timestr & 
-                                file.timestr <= time.info$nhrs.yrdoy]
-      } # end if
-    }
+    
+    # select the ones that need for scaling GPP 
+    csif.files <- list.files(path = csif.path, pattern = '.nc', 
+                             recursive = T, full.names = T)
+    if (is.null(nhrs)) {
+      csif.file <- csif.files[grep(search.timestr, csif.files)]
+
+    } else {
+      ref.csif.files <- gsub('.v2', '', csif.files)
+      file.timestr <- strsplit.to.df(basename(ref.csif.files), sep = '\\.')$V5
+      csif.file <- csif.files[file.timestr >= search.timestr & 
+                              file.timestr <= time.info$nhrs.yrdoy]
+    } # end if
+
     if (length(csif.file) == 0) {
         cat(paste('grab.csif(): No CSIF file found for', search.timestr, 
                   ', please check...\n')); return()
@@ -41,6 +37,7 @@ grab.csif <- function(csif.path, timestr, sif.temp, TA.path, TA.varname,
       # grab and crop CSIF according to 'var'
       sel.csif <- crop(stack(csif.file, varname = var), ext)
       if (nlayers(sel.csif) > 1) sel.csif <- mean(sel.csif)
+      
       #Also grab and crop temperature data
       # Load hourly TA (deg C) and incoming SW from ERA5
       # use nhrs = 23 to grab all hourly variables in a day 
@@ -52,17 +49,17 @@ grab.csif <- function(csif.path, timestr, sif.temp, TA.path, TA.varname,
       # find the start time of the 4-day interval that "timestr" fall into 
       find.timestr <- timestr4[findInterval(time.info$find.timestr, timestr4)]
       
-      #temp.timestr<-rep(time.info$max.timestr,24)
-      #cat(paste('\ngrab.csif(): Loading hourly TA for', timestr, '\n'))
-      if (sif.temp==TRUE) {     # hourly air temp in UTC
+      #If it is outside of the growing season set SIF to 0
+      if ((search.timestr-yr2*1000)<90 | (search.timestr-yr2*1000)>340){     # hourly air temp in UTC
+        sel.csif<-sel.csif*0
+      }else if (sif.temp==TRUE) {     # hourly air temp in UTC
         TA.brk <- prep.era5(TA.path, TA.varname, timestr = find.timestr, 
-                              ext, nhrs = 24 * 4)
+                            ext, nhrs = 24 * 4)
         TA.pj   <- raster::projectRaster(TA.brk, sel.csif) # in degC
         
-        TA.mean <- mean(TA.pj)
-        sel.csif[0 >= TA.mean]<-TA.mean[0 >= TA.mean]*0
+        TA.mean <- mean(TA.pj) #If mean air temperature is <0
+        sel.csif[0 >= TA.mean]<-TA.mean[0 >= TA.mean]*0 #set SIF to 0
       }
-      
 
       if (form == 'df') {
         sel.csif <- raster::as.data.frame(sel.csif, xy = T)
@@ -73,4 +70,3 @@ grab.csif <- function(csif.path, timestr, sif.temp, TA.path, TA.varname,
     } # end if 
 
 } # end of subroutine
-
